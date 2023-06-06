08aae815d7d90c18800b2aee6dc9f0b948d0e97e
Night Owl <91905208+nightowl121@users.noreply.github.com>
2023-06-06 16:48:47 +0545

    feat: add cosmwasm deployment workflow (#11)
    * feat: add schema of cw-hub-bnusd
    
    * feat: add archway as submodule
    
    * feat: add cosmwasm-check as dev dependencies
    
    * feat: add scripts for common tasks
    
    * feat: add workflow to deploy wasm contracts
    
    * feat: remove cosmwasm-check from dev dependencies and install as a tool
    
    * feat: use 1.69.0 rust version
    
    * install wasm-opt and cw-check in ci
    
    * fix: add missing rust flags
    
    * update package versions
    
    * feat: add installation packages as components
    
    * feat: use correct container name
    
    * feat: separate out copy command
    
    * feat: use sh command to run the script
    
    * feat: move caching option to first step
    
    * feat: install wasm-opt using downloaded file

---
+++ scripts/generate_wasm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# install wasm-opt
+BINARYEN_VERS=110
+BINARYEN_DWN="https://github.com/WebAssembly/binaryen/releases/download/version_${BINARYEN_VERS}/binaryen-version_${BINARYEN_VERS}-x86_64-linux.tar.gz"
+
+if ! which wasm-opt; then
+  curl -OL $BINARYEN_DWN
+  tar xf binaryen-version_${BINARYEN_VERS}-x86_64-linux.tar.gz
+  export PATH=$PATH:$PWD/binaryen-version_${BINARYEN_VERS}/bin
+fi
+
+# Generate optimized wasm files and verify generated wasm with cosmwasm-check
+mkdir -p artifacts
+RUSTFLAGS='-C link-arg=-s' cargo wasm
+for WASM in ./target/wasm32-unknown-unknown/release/*.wasm; do
+  NAME=$(basename "$WASM" .wasm)${SUFFIX}.wasm
+  echo "########Creating intermediate hash for $NAME ...########"
+  sha256sum -- "$WASM" | tee -a artifacts/checksums_intermediate.txt
+  echo "########Optimizing $NAME ...########"
+  wasm-opt -Oz "$WASM" -o "artifacts/$NAME"
+  echo "########Verifying $NAME file with cosmwasm-check ...########"
+  cosmwasm-check "artifacts/$NAME"
+done
---
+++ scripts/pre_commit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+cargo fmt --all
+#cargo clippy --fix
+source ./scripts/run_in_subprojects.sh ./contracts/token-contracts/cw-hub-bnusd
+cargo clean
