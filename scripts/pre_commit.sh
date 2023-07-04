52b309c8a559d61431076112c2fa7fda7cc2289f
Night Owl <nightowl121@protonmail.com>
2023-07-04 14:48:26 +0545

    feat: add clippy command in pre commit script
    Signed-off-by: Night Owl <nightowl121@protonmail.com>

--- scripts/pre_commit.sh
+++ scripts/pre_commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cargo clippy --fix
 cargo fmt --all
+cargo clippy --fix
 source ./scripts/run_in_subprojects.sh ./contracts/token-contracts/cw-hub-bnusd ./contracts/core-contracts/cw-asset-manager
 cargo clean
