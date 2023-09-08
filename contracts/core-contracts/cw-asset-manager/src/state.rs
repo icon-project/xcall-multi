e4ab9ef542c0e1c39657855a131de7a2ea91f4cc
Night Owl <nightowl121@protonmail.com>
2023-09-08 21:02:29 +0545

    feat: add setup method during contract instantiate
    Signed-off-by: Night Owl <nightowl121@protonmail.com>

--- contracts/core-contracts/cw-asset-manager/src/state.rs
+++ contracts/core-contracts/cw-asset-manager/src/state.rs
@@ -9,6 +9,5 @@ pub const SOURCE_XCALL: Item<Addr> = Item::new("source_xcall_address");
 pub const X_CALL_NETWORK_ADDRESS: Item<NetworkAddress> = Item::new("source_xcall_network_address");
 pub const NID: Item<NetId> = Item::new("network_id");
 
-pub const ICON_ASSET_MANAGER: Item<NetworkAddress> =
-    Item::new("icon_asset_manager_network_address");
+pub const ICON_ASSET_MANAGER: Item<Addr> = Item::new("icon_asset_manager_network_address");
 pub const ICON_NET_ID: Item<NetId> = Item::new("icon_asset_manager_network_id");
