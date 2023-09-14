a211079d7a86751d6cc308d243ebd54193b33e9a
Night Owl <nightowl121@protonmail.com>
2023-09-14 13:44:50 +0545

    fix: path of wasm file
    Signed-off-by: Night Owl <nightowl121@protonmail.com>

--- scripts/generate_wasm.sh
+++ scripts/generate_wasm.sh
@@ -32,24 +32,23 @@ RUSTFLAGS='-C link-arg=-s' cargo wasm
 for WASM in ./target/wasm32-unknown-unknown/release/*.wasm; do
   NAME=$(basename "$WASM" .wasm)${SUFFIX}.wasm
   echo "########Creating intermediate hash for $NAME ...########"
-  sha256sum -- "$WASM" | tee -a artifacts/checksums_intermediate.txt
+  sha256sum -- "$WASM" | tee -a artifacts/archway/checksums_intermediate.txt
   echo "########Optimizing $NAME ...########"
-  wasm-opt -Oz "$WASM" -o "artifacts/$NAME"
+  wasm-opt -Oz "$WASM" -o "artifacts/archway/$NAME"
   echo "########Verifying $NAME file with cosmwasm-check ...########"
 done
 
-cosmwasm-check "artifacts/cw_asset_manager.wasm"
-cosmwasm-check "artifacts/cw_hub_bnusd.wasm"
+cosmwasm-check "artifacts/archway/cw_asset_manager.wasm"
+cosmwasm-check "artifacts/archway/cw_hub_bnusd.wasm"
 
 # validate size
 echo "Check if size of wasm file exceeds $MAX_WASM_SIZE kilobytes..."
-for file in artifacts/archway/*.wasm
-do
-size=$(du -k "$file" | awk '{print $1}')
-if [ "$size" -gt $MAX_WASM_SIZE ]; then
-echo "Error: $file : $size KB has exceeded maximum contract size limit of $MAX_WASM_SIZE KB."
-exit 1
-fi
-echo "$file : $size KB"
+for file in artifacts/archway/*.wasm; do
+  size=$(du -k "$file" | awk '{print $1}')
+  if [ "$size" -gt $MAX_WASM_SIZE ]; then
+    echo "Error: $file : $size KB has exceeded maximum contract size limit of $MAX_WASM_SIZE KB."
+    exit 1
+  fi
+  echo "$file : $size KB"
 done
 echo "The size of all contracts is well within the $MAX_WASM_SIZE KB limit."
