d3c2121d75aa00585490bfcee904ac365500de59
Sudeep Bhandari <bhandari.sudeep68@gmail.com>
2023-09-18 14:20:26 +0545

    feat: setup foundry structure (#79)
    * remove msg encoding in encodeCSMessageResponse
    
    * chore: forge init
    
    * forge install: forge-std
    
    v1.6.1
    
    * initial foundry setup
    
    * forge install: openzeppelin-contracts-upgradeable
    
    v4.9.3
    
    * forge install: openzeppelin-contracts
    
    v4.9.3
    
    * add contract setup script for Callservice for Xcall
    
    * add initial test setup for Xcall

---
+++ contracts/evm/xcall/script/CallService.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Script, console2} from "forge-std/Script.sol";
+import "../contracts/CallService.sol";
+
+contract CallServiceScript is Script {
+    function setUp() public {}
+
+    function run() public {
+        vm.broadcast();
+        new CallService();
+        
+    }
+}
