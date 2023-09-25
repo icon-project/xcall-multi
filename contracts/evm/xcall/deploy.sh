35d413b27ffd131ac835357dedf0e39df0235f1c
qwerty0789 <qa5505815@gmail.com>
2023-09-26 02:01:53 +0545

    fix: deployment script updated

---
+++ contracts/evm/xcall/deploy.sh
@@ -0,0 +1,2 @@
+source .env
+forge script script/CallService.s.sol:CallServiceScript --fork-url $BINANCE_TESTNET_RPC_URL --broadcast
--- contracts/evm/xcall/script/CallService.s.sol
+++ contracts/evm/xcall/script/CallService.s.sol
@@ -8,8 +8,14 @@ contract CallServiceScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.broadcast();
-        new CallService();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        CallService xcall = new CallService();
+
+        xcall.initialize("bsc");
+
+        vm.stopBroadcast();
         
     }
 }
