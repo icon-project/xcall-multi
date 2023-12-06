1433c8d58f1cec536e3bf01ff2a7146b114d6a48
gcranju <qa5505815@gmail.com>
2023-12-06 10:07:03 +0545

    fix: review comments addressed for solidity part

--- contracts/evm/contracts/adapters/CentralizedConnection.sol
+++ contracts/evm/contracts/adapters/CentralizedConnection.sol
@@ -7,99 +7,130 @@ import "@xcall/utils/Types.sol";
 import "@xcall/contracts/xcall/interfaces/IConnection.sol";
 import "@iconfoundation/btp2-solidity-library/interfaces/ICallService.sol";
 import {console2} from "forge-std/Test.sol";
-import "@iconfoundation/btp2-solidity-library/utils/Strings.sol";
-import "@iconfoundation/btp2-solidity-library/utils/ParseAddress.sol";
+pragma solidity ^0.8.0;
 
 
 contract CentralizedConnection is Initializable, IConnection {
-    using Strings for string;
-    using ParseAddress for address;
-    using ParseAddress for string;
 
     mapping(string => uint256) private messageFees;
     mapping(string => uint256) private responseFees;
-
-    mapping(bytes32 => bool) public seenDeliveryVaaHashes;
+    mapping(string => mapping(uint256 => uint256)) receipts;
     address private xCall;
-    address private adminAddress;
+    //relayer = admin
     address private relayer;
 
 
-    event Message(string targetNetwork,int256 sn,bytes msg);
+    event Message(string targetNetwork,int256 sn,bytes _msg);
 
 
-    modifier onlyAdmin() {
-        require(msg.sender == this.admin(), "OnlyAdmin");
+    modifier onlyRelayer() {
+        require(msg.sender == this.admin(), "OnlyRelayer");
         _;
     }
 
     function initialize(address _relayer, address _xCall) public initializer {
-        adminAddress = msg.sender;
         xCall = _xCall;
         relayer = _relayer;
     }
 
-    function setRelayer(address _relayer) external onlyAdmin {
-        relayer = _relayer;
-    }
-
-    function getRelayer() public view returns (address _relayer) {
-        return relayer;
-    }
-
+    /**
+     @notice Sets the fee to the target network
+     @param networkId String Network Id of target chain
+     @param messageFee Integer ( The fee needed to send a Message )
+     @param responseFee Integer (The fee of the response )
+     */
     function setFee(
         string calldata networkId,
         uint256 messageFee,
         uint256 responseFee
-    ) external onlyAdmin {
+    ) external onlyRelayer {
         messageFees[networkId] = messageFee;
         responseFees[networkId] = responseFee;
     }
 
-
-
-    function getFee(string memory _to, bool _response) external view override returns (uint256 _fee) {
-        uint256 messageFee = messageFees[_to];
-        if (_response == true) {
-            uint256 responseFee = responseFees[_to];
+    /**
+     @notice Gets the fee to the target network
+    @param to       String Network Id of target chain
+    @param response Boolean ( Whether the responding fee is included )
+    @return fee Integer (The fee of sending a message to a given destination network )
+    */
+    function getFee(string memory to, bool response) external view override returns (uint256 fee) {
+        uint256 messageFee = messageFees[to];
+        if (response == true) {
+            uint256 responseFee = responseFees[to];
             return messageFee + responseFee;
         }
         return messageFee;
     }
 
-
+    /**
+     @notice Sends the message to a specific network.
+     @param sn : positive for two-way message, zero for one-way message, negative for response
+     @param to  String ( Network Id of destination network )
+     @param svc String ( name of the service )
+     @param sn  Integer ( serial number of the message )
+     @param _msg Bytes ( serialized bytes of Service Message )
+     */
     function sendMessage(
-        string calldata _to,
-        string calldata _svc,
-        int256 _sn,
+        string calldata to,
+        string calldata svc,
+        int256 sn,
         bytes calldata _msg
     ) external override payable {
         console2.log(msg.sender);
-        require(msg.sender == xCall, Strings.concat(msg.sender.toString(), (xCall).toString()));
-        uint256 fee = this.getFee(_to,false);
-        require(msg.value >= fee,"Fee is not Sufficient"); 
-        emit Message(_to, _sn, _msg);
+        require(msg.sender == xCall, "Only Xcall can call sendMessage");
+        emit Message(to, sn, _msg);
     }
-
-    function recvMessage(
-        string memory srcNID,
-        string memory sn,
-        bytes calldata _msg
-    ) public {
-        require(msg.sender==relayer, "Only relayer can call this function");
-        bytes32 hash = keccak256(abi.encodePacked(_msg, sn));
-        require(!seenDeliveryVaaHashes[hash], "Message already processed");
-        seenDeliveryVaaHashes[hash] = true;
-        ICallService(xCall).handleMessage(srcNID, _msg);
-    }
-
 
     /**
-   * @notice Set the address of the admin.
-     * @param _address The address of the admin.
+     @notice Sends the message to a xCall.
+     @param srcNetwork  String ( Network Id )
+     @param sn Integer ( serial number of the message )
+     @param _msg Bytes ( serialized bytes of Service Message )
      */
-    function setAdmin(address _address) external onlyAdmin {
-        adminAddress = _address;
+    function recvMessage(
+        string memory srcNetwork,
+        uint256 sn,
+        bytes calldata _msg
+    ) public onlyRelayer {
+        require(msg.sender==relayer, "Only relayer can call this function");
+        require(receipts[srcNetwork][sn]==0,"Duplicate Message");
+        receipts[srcNetwork][sn]=1;
+        ICallService(xCall).handleMessage(srcNetwork, _msg);
+    }
+
+    /**
+     @notice Sends the balance of the contract to the owner(relayer)
+
+    */
+    function claimFees() public onlyRelayer {
+        payable(relayer).transfer(address(this).balance);
+    }
+
+    /**
+     @notice Revert a messages, used in special cases where message can't just be dropped
+     @param sn  Integer ( serial number of the message )
+     */
+    function revertMessage(uint256 sn) public onlyRelayer {
+        ICallService(xCall).handleError(sn);
+    }
+
+    /**
+     @notice Gets a message receipt
+     @param srcNetwork String ( Network Id )
+     @param sn Integer ( serial number of the message )
+     @return boolean if is has been recived or not
+     */
+    function getReceipt(string memory srcNetwork, uint256 sn) public view returns (uint256) {
+        return receipts[srcNetwork][sn];
+    }
+
+    /**
+        @notice Set the address of the admin.
+        @param _address The address of the admin.
+     */
+    function setAdmin(address _address) external onlyRelayer {
+        relayer = _address;
     }
 
     /**
@@ -110,6 +141,8 @@ contract CentralizedConnection is Initializable, IConnection {
     ) external view returns (
         address
     ) {
-        return adminAddress;
+        return relayer;
     }
+
+
 }
