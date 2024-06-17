31edbf67b1c5ceedaa8db33a21a0e571065a463e
bishalbikram <contact.bishalbikram@gmail.com>
2024-06-17 11:10:15 +0545

    add: create handle call instructions

---
+++ contracts/solana/programs/xcall/src/instructions/config.rs
@@ -0,0 +1,66 @@
+use anchor_lang::prelude::*;
+
+use crate::state::*;
+
+#[derive(Accounts)]
+pub struct ConfigCtx<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = Config::INIT_SPACE,
+        seeds = [Config::SEED_PREFIX.as_bytes()],
+        bump
+    )]
+    pub config: Account<'info, Config>,
+
+    #[account(
+        init,
+        payer = signer,
+        space = 8 + 1024,
+        seeds = [Reply::SEED_PREFIX.as_bytes()],
+        bump
+    )]
+    pub reply: Account<'info, Reply>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateConfigCtx<'info> {
+    #[account(
+        mut,
+        seeds = [Config::SEED_PREFIX.as_bytes()],
+        bump,
+    )]
+    pub config: Account<'info, Config>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+#[instruction(network_id: String)]
+pub struct DefaultConnectionCtx<'info> {
+    #[account(
+        init,
+        payer = signer,
+        space = DefaultConnection::INIT_SPACE,
+        seeds = [DefaultConnection::SEED_PREFIX.as_bytes(), network_id.as_bytes()],
+        bump
+    )]
+    pub default_connection: Account<'info, DefaultConnection>,
+
+    #[account(
+        seeds = [Config::SEED_PREFIX.as_bytes()],
+        bump
+    )]
+    pub config: Account<'info, Config>,
+
+    #[account(mut)]
+    pub signer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+}
---
+++ contracts/solana/programs/xcall/src/instructions/mod.rs
@@ -0,0 +1,7 @@
+pub mod config;
+pub mod handle_message;
+pub mod send_message;
+
+pub use config::*;
+pub use handle_message::*;
+pub use send_message::*;
