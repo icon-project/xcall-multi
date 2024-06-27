b539d4f990bc8c09249f4e7aa4d131afcb775c6e
nanney <nanneychan03@gmail.com>
2024-06-27 15:08:20 +0545

    chore: rollback methods optimized

--- contracts/solana/programs/xcall/src/instructions/execute_rollback.rs
+++ contracts/solana/programs/xcall/src/instructions/execute_rollback.rs
@@ -1,14 +1,7 @@
-use std::{str::FromStr, vec};
 
-use anchor_lang::{
-    prelude::*, solana_program::{
-        hash, instruction::Instruction, program::{get_return_data, invoke_signed},
-    }
-};
+use crate::execute_call::handle_call_message;
 
-use xcall_lib::{message::msg_type::MessageType, 
-    network_address::NetworkAddress, 
-    state::CpiDappResponse};
+use anchor_lang::prelude::*;
 
 use crate::{
     error::XcallError, event, state::RollbackAccount, DefaultConnection 
@@ -32,14 +25,7 @@ pub fn execute_rollback<'a, 'b, 'c, 'info>(
     let data = &req.rollback.rollback().to_vec();
     let protocols = req.rollback.protocols().to_vec();
     
-    pub struct CSMessageRequest {
-    from: NetworkAddress,
-    to: String,
-    sequence_no: u128,
-    msg_type: MessageType,
-    data: Vec<u8>, // TODO: cosmos this is nullable??
-    protocols: Vec<String>,
-}
+
     let signer =  &ctx.accounts.signer;
     let program = &ctx.accounts.system_program;
     let remaining_accounts = ctx.remaining_accounts;
@@ -62,84 +48,12 @@ pub fn execute_rollback<'a, 'b, 'c, 'info>(
     Ok(())
 }
 
-
-
-#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
-pub struct DappArgs {
-    pub from: String,
-    pub data: Vec<u8>,
-    pub protocols: Vec<String>,
-}
-
-pub fn handle_call_message<'info>(
-    signer : &Signer<'info>,
-    program : &Program<'info,System>,
-    remaining_accounts :&[AccountInfo<'info>],
-    signers_seeds: &[&[&[u8]]],
-    to: &String,
-    from: &NetworkAddress,
-    _data : &Vec<u8>,
-    protocols: Vec<String>
-
-) -> Result<CpiDappResponse>{
-
-    let ix_name = format!("{}:{}", "global", "handle_call_message");
-    let ix_discriminator = hash::hash(ix_name.as_bytes()).to_bytes()[..8].to_vec();
-
-    let mut data = vec![];
-    let args = DappArgs {
-        from: from.to_string(),
-        data: _data.clone(),
-        protocols: protocols
-    };
-    args.serialize(&mut data)?;
-
-    let mut ix_data = Vec::new();
-    ix_data.extend_from_slice(&ix_discriminator);
-    ix_data.extend_from_slice(&data);
-
-    let mut account_metas: Vec<AccountMeta> = vec![
-        AccountMeta::new(signer.key(), true),
-        AccountMeta::new_readonly(program.key(), false),
-
-    ];
-
-    let mut account_infos:Vec<AccountInfo<'info>> = vec![
-        signer.to_account_info(),
-        program.to_account_info(),
-        ];
-
-    for accounts in remaining_accounts {
-        account_metas.push(AccountMeta::new(accounts.key(), accounts.is_signer));
-        account_infos.push(accounts.to_account_info());
-    }
-
-    let ix = Instruction {
-        program_id: Pubkey::from_str(to).unwrap(),
-        accounts: account_metas,
-        data: ix_data,
-    };
-
-    invoke_signed(&ix, &account_infos, signers_seeds)?;
-
-    let (_, data) = get_return_data().unwrap();
-    
-
-    let mut data_slice : &[u8] = &data;
-    let data_ref : &mut &[u8] = &mut data_slice;
-    let deserialized = CpiDappResponse::deserialize(data_ref).unwrap();
-
-    Ok(deserialized)
-
-}
-
-
 #[derive(Accounts)]
 #[instruction(_sn : u128,)]
 pub struct ExecuteRollbackCtx<'info> {
     #[account(
         mut, 
-        seeds = [b"rollback", &_sn.to_le_bytes()], 
+        seeds = [RollbackAccount::SEED_PREFIX.as_bytes(), &_sn.to_string().as_bytes()], 
         bump = rollback.bump , 
         close = owner
     )]
@@ -147,7 +61,7 @@ pub struct ExecuteRollbackCtx<'info> {
 
     #[account(
         mut,
-        seeds = ["conn".as_bytes()],
+        seeds = [DefaultConnection::SEED_PREFIX.as_bytes()],
         bump
     )]
     pub default_connection : Option<Account<'info,DefaultConnection>>,
--- contracts/solana/programs/xcall/src/instructions/mod.rs
+++ contracts/solana/programs/xcall/src/instructions/mod.rs
@@ -2,10 +2,11 @@ pub mod config;
 pub mod handle_message;
 pub mod send_message;
 pub mod execute_call;
-mod execute_rollback;
+pub mod execute_rollback;
 
 pub use config::*;
 pub use handle_message::*;
 pub use send_message::*;
 pub use execute_call::*;
+pub use execute_rollback::*;
 
