a280d9d25ba13bf7f057ea2440250a060a55619c
bishalbikram <contact.bishalbikram@gmail.com>
2024-07-01 13:59:57 +0545

    add: xcall and connection args type

--- contracts/solana/libs/xcall-lib/src/state.rs
+++ contracts/solana/libs/xcall-lib/src/state.rs
@@ -1,9 +1,8 @@
 use anchor_lang::prelude::borsh;
 use anchor_lang::{AnchorDeserialize, AnchorSerialize};
 
-
-#[derive(AnchorSerialize,AnchorDeserialize,Debug)]
+#[derive(AnchorSerialize, AnchorDeserialize, Debug)]
 pub struct CpiDappResponse {
     pub success: bool,
-    pub data: Option<String>
+    pub data: Option<String>,
 }
