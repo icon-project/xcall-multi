395acc78f01174a0f02d552dfd534f4e13b64d1b
sherpalden <sherpalden369@gmail.com>
2024-09-23 18:04:11 +0545

    test: submitSignature

--- contracts/javascore/aggregator/src/main/java/relay/aggregator/RelayAggregator.java
+++ contracts/javascore/aggregator/src/main/java/relay/aggregator/RelayAggregator.java
@@ -16,10 +16,10 @@
 
 package relay.aggregator;
 
-import score.Context;
 
 import java.math.BigInteger;
 
+import score.Context;
 import score.Address;
 import score.VarDB;
 import score.DictDB;
@@ -28,13 +28,13 @@ import score.BranchDB;
 import score.annotation.External;
 
 public class RelayAggregator {
-    protected final VarDB<Address> admin = Context.newVarDB("admin", Address.class);
+    private final VarDB<Address> admin = Context.newVarDB("admin", Address.class);
 
-    protected final DictDB<Address, Boolean> relayers = Context.newDictDB("relayers", Boolean.class);
+    private final DictDB<Address, Boolean> relayers = Context.newDictDB("relayers", Boolean.class);
 
-    protected final BranchDB<String, DictDB<BigInteger, byte[]>> packets = Context.newBranchDB("packets", byte[].class);
+    private final BranchDB<String, DictDB<BigInteger, byte[]>> packets = Context.newBranchDB("packets", byte[].class);
 
-    // protected final BranchDB<String, DictDB<BigInteger, String[]>> signatures = Context.newBranchDB("signatures", String[].class);
+    private final BranchDB<String, BranchDB<BigInteger, DictDB<Address, String>>> signatures = Context.newBranchDB("signatures", DictDB.class);
 
     public RelayAggregator(Address _admin, Address[] _relayers) {
         if (admin.get() == null) {
@@ -48,20 +48,60 @@ public class RelayAggregator {
     /**
      * Registers a new packet.
      *
-     * @param nid  the network ID
-     * @param sn   the sequence number
-     * @param data the packet data
+     * @param nid network ID
+     * @param sn sequence number
+     * @param data packet data
      */
     @External
     public void registerPacket(String nid, BigInteger sn, byte[] data) {
         adminOnly();
-        DictDB<BigInteger, byte[]> packetDict = packets.at(nid);
+        DictDB<BigInteger, byte[]> packetDict = getPackets(nid);
         byte[] pkt = packetDict.get(sn);
-        Context.require(pkt == null, "Packet already exists.");
+        Context.require(pkt == null, "Packet already exists");
 
         packetDict.set(sn, data);
     }
 
+    /**
+     * Submits a signature for the registered packet.
+     *
+     * @param nid network ID
+     * @param sn sequence number
+     * @param signature packet signature
+     */
+    @External
+    public void submitSignature(String nid, BigInteger sn, String signature) {
+        relayersOnly();
+
+        DictDB<BigInteger, byte[]> packetDict = getPackets(nid);
+        byte[] packetData = packetDict.get(sn);
+        Context.require(packetData != null, "Packet does not exist");
+
+
+        DictDB<Address, String> addressSigns = getSignatures(nid, sn);
+
+        Address caller = Context.getCaller();
+        String sign = addressSigns.get(caller);
+        Context.require(sign == null, "Signature already exists.");
+
+        addressSigns.set(caller, signature);
+    }
+
+    /**
+     * Retrieves the signatures dictionary for the packet.
+     *
+     * @param nid network ID of the source chain
+     * @param sn sequence number of the source chain message
+     * @return map of relayer addresses to signatures
+     */
+    protected DictDB<Address, String> getSignatures(String nid, BigInteger sn) {
+        BranchDB<BigInteger, DictDB<Address, String>> snSigns = signatures.at(nid);
+        if (snSigns == null) {
+            snSigns = Context.newBranchDB(nid+"-signs", DictDB.class);
+        }
+        return snSigns.at(sn);
+    }
+    
 
     /**
      * Sets the admin address.
@@ -77,11 +117,21 @@ public class RelayAggregator {
     /**
      * Retrieves the admin address.
      *
-     * @return The admin address.
+     * @return admin address.
      */
     @External(readonly = true)
     public Address getAdmin() {
         return admin.get();
+    }
+
+    /**
+     * Retrieves the packets dictionary.
+     * *
+     * @param nid network id of the source chain
+     * @return list of mapping from sn to packet data.
+     */
+    protected DictDB<BigInteger, byte[]> getPackets(String nid) {
+        return packets.at(nid);
     }
 
     /**
@@ -90,6 +140,17 @@ public class RelayAggregator {
      * @return true if the caller is the admin, false otherwise
      */
     private void adminOnly() {
-        Context.require(Context.getCaller().equals(admin.get()), "Unauthorized to call this method");
+        Context.require(Context.getCaller().equals(admin.get()), "Unauthorized: caller is not the leader relayer");
+    }
+
+    /**
+     * Checks if the caller of the function is among the relayers.
+     *
+     * @return true if the caller is among the relayers, otherwise false
+     */
+    private void relayersOnly() {
+        Address caller = Context.getCaller();
+        boolean isRelayer = relayers.get(caller);
+        Context.require(isRelayer, "Unauthorized: caller is not a registered relayer");
     }
 }
--- contracts/javascore/aggregator/src/test/java/relay/aggregator/RelayAggregatorTest.java
+++ contracts/javascore/aggregator/src/test/java/relay/aggregator/RelayAggregatorTest.java
@@ -1,11 +1,25 @@
 package relay.aggregator;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.PrivateKey;
+import java.security.Security;
+import java.security.AlgorithmParameters;
+import java.security.KeyFactory;
+import java.security.Signature;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import java.security.spec.ECPrivateKeySpec;
+import java.security.spec.ECGenParameterSpec;
+import java.security.spec.ECParameterSpec;
+
 import score.Address;
 import score.UserRevertedException;
+import score.DictDB;
 
 import com.iconloop.score.test.Account;
 import com.iconloop.score.test.Score;
@@ -14,20 +28,34 @@ import com.iconloop.score.test.TestBase;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class RelayAggregatorTest extends TestBase {
-    protected final ServiceManager sm = getServiceManager();
+    private final ServiceManager sm = getServiceManager();
 
-    protected final Account adminAc = sm.createAccount();
-    protected final Account relayerAcOne = sm.createAccount();
-    protected final Account relayerAcTwo = sm.createAccount();
+    private final Account adminAc = sm.createAccount();
 
-    protected Score aggregator;
+    private final Account relayerAcOne = sm.getAccount(new Address(HexString.toBytes("hxe794bcf6a92efb5e0b58cfc4728236c650d86dce")));
+    private final String relayerAcOnePkey = "40b90166d6ace4acbdc59596fd483e487bf58ec4ae5ff31e4f97f039af5b23f7";
+    
+    private final Account relayerAcTwo = sm.getAccount(new Address(HexString.toBytes("hx3445c3d4341a9b1fc2ae6fd578a4453ab0072c07")));
+    private final String relayerAcTwoPkey = "0xb533387c502c39eea62f4c2a5be31e388fa87313438236656b4c71be92fed066";
+
+    private Score aggregator;
+    private RelayAggregator aggregatorSpy;
 
     @BeforeEach
     void setup() throws Exception {
         Address[] relayers = new Address[]{relayerAcOne.getAddress(), relayerAcTwo.getAddress()};
         aggregator = sm.deploy(adminAc, RelayAggregator.class, adminAc.getAddress(), relayers);
+
+        aggregatorSpy = (RelayAggregator) spy(aggregator.getInstance());
+        aggregator.setInstance(aggregatorSpy);
+
+        Security.addProvider(new BouncyCastleProvider());
     }
 
     @Test
@@ -47,6 +75,106 @@ class RelayAggregatorTest extends TestBase {
         Executable action = () -> aggregator.invoke(normalAc, "setAdmin", newAdminAc.getAddress());
         UserRevertedException e = assertThrows(UserRevertedException.class, action);
     
-        assertEquals("Reverted(0): Unauthorized to call this method", e.getMessage());
+        assertEquals("Reverted(0): Unauthorized: caller is not the leader relayer", e.getMessage());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testRegisterPacket() {
+        String nid = "0x2.icon";
+        BigInteger sn = BigInteger.ONE;
+        byte[] data = new byte[]{0x01, 0x02};
+    
+        DictDB<BigInteger, byte[]> mockDictDB = mock(DictDB.class);
+
+        when(aggregatorSpy.getPackets(nid)).thenReturn(mockDictDB);
+
+        aggregator.invoke(adminAc, "registerPacket", nid, sn, data);
+
+        verify(mockDictDB).set(sn, data);
+    }
+
+    @Test
+    public void testRegisterPacket_duplicate() {
+        String nid = "0x2.icon";
+        BigInteger sn = BigInteger.ONE;
+        byte[] data = new byte[]{0x01, 0x02};
+
+        aggregator.invoke(adminAc, "registerPacket", nid, sn, data);
+
+        Executable action = () -> aggregator.invoke(adminAc, "registerPacket", nid, sn, data);
+        UserRevertedException e = assertThrows(UserRevertedException.class, action);
+    
+        assertEquals("Reverted(0): Packet already exists", e.getMessage());
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    public void testSubmitSignature() throws Exception {
+        String nid = "0x2.icon";
+        BigInteger sn = BigInteger.ONE;
+        byte[] data = new byte[]{0x01, 0x02};
+
+        aggregator.invoke(adminAc, "registerPacket", nid, sn, data);
+    
+        DictDB<Address, String> mockSignatures = mock(DictDB.class);
+        
+
+        when(aggregatorSpy.getSignatures(nid, sn)).thenReturn(mockSignatures);
+
+        byte[] dataHash = hashData(data);
+        String sign = signData(dataHash,relayerAcOnePkey);
+
+        aggregator.invoke(relayerAcOne, "submitSignature", nid, sn, sign);
+
+        verify(mockSignatures).set(relayerAcOne.getAddress(), sign);
+    }
+
+    private static byte[] hashData(byte[] data) throws Exception {
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        return digest.digest(data);
+    }
+
+    private static String signData(byte[] dataHash, String pKeyStr) throws Exception {
+        byte[] pKeyBytes = HexString.toBytes(pKeyStr);
+        
+        PrivateKey pKey = parsePrivateKey(pKeyBytes);
+
+        Signature signature = Signature.getInstance("SHA256withECDSA", "BC");
+        signature.initSign(pKey);
+        signature.update(dataHash);
+        
+        byte[] sign = signature.sign();
+        return HexString.fromBytes(sign);
+    }
+
+    public static PrivateKey parsePrivateKey(byte[] pKeyBytes) throws Exception {
+        // Ensure the private key is 32 bytes
+        if (pKeyBytes.length != 32) {
+            throw new IllegalArgumentException("Invalid private key length: must be 32 bytes.");
+        }
+
+        // Create a BigInteger from the private key bytes (unsigned)
+        BigInteger privateKeyInt = new BigInteger(1, pKeyBytes);
+
+        // Create EC PrivateKeySpec using the secp256k1 curve parameters
+        ECParameterSpec ecSpec = getSecp256k1Curve();
+        ECPrivateKeySpec privateKeySpec = new ECPrivateKeySpec(privateKeyInt, ecSpec);
+
+        // Create the KeyFactory for generating EC keys
+        KeyFactory keyFactory = KeyFactory.getInstance("ECDSA", "BC");
+
+        // Generate the private key from the spec
+        return keyFactory.generatePrivate(privateKeySpec);
+    }
+
+    private static ECParameterSpec getSecp256k1Curve() {
+        try {
+            AlgorithmParameters parameters = AlgorithmParameters.getInstance("EC", "BC");
+            parameters.init(new ECGenParameterSpec("secp256k1"));
+            return parameters.getParameterSpec(ECParameterSpec.class);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to get secp256k1 curve parameters", e);
+        }
     }
 }
