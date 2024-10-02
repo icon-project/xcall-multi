40110bbe8a4f2d242558367ce5921fe54c9ddce2
gcranju <134275268+gcranju@users.noreply.github.com>
2024-10-02 16:08:59 +0545

    fix: xcall lib id change

--- contracts/solana/programs/xcall/src/lib.rs
+++ contracts/solana/programs/xcall/src/lib.rs
@@ -18,7 +18,7 @@ use xcall_lib::{
     query_account_type::{QueryAccountsPaginateResponse, QueryAccountsResponse},
 };
 
-declare_id!("6PDxNoATKJpTdkEEiiKQucnhUJYvhZg3BNVckyV8sS1q");
+declare_id!("GjmGXdyHoJohGvg5k2PUxBLtgfanRLfTXHWLXWDD6BZY");
 
 #[program]
 pub mod xcall {
