874b68f6e2501f9fd55c12cab962e6c25aed8f18
CyrusVorwald <90732384+CyrusVorwald@users.noreply.github.com>
2024-11-20 21:15:45 -0500

    fix: change rlp to match xcall instead of ethereum

--- contracts/stacks/lib/rlp/rlp-encode.clar
+++ contracts/stacks/lib/rlp/rlp-encode.clar
@@ -3,31 +3,34 @@
 )
 
 (define-read-only (encode-string (message (string-ascii 1024)))
-    (let
-        (   (encoded (unwrap-panic (to-consensus-buff? message)))
-        )
-        (if (is-eq (len encoded) u5)  ;; Empty string is type byte + 4 length bytes
-            0x80
-            (let (
-                (encoded-length (- (len encoded) u5))
-                (string-content (unwrap-panic (slice? encoded u5 (len encoded))))
-            )
-                (if (> encoded-length u55)
-                    (let (
-                        (length-byte (unwrap-panic (element-at? (unwrap-panic (to-consensus-buff? encoded-length)) u16)))
-                        (prefix (concat 0xb8 length-byte))
-                    )
-                    (check_length (concat prefix string-content)))
-                    (let (
-                        (id (unwrap-panic (to-consensus-buff? (+ u128 encoded-length))))
-                        (prefix (unwrap-panic (element-at? id u16)))
-                        (res (concat prefix string-content))
-                    )
-                    (check_length res))
-                )
-            )
-        )
-    )
+   (let
+       (   (encoded (unwrap-panic (to-consensus-buff? message)))
+       )
+       (if (is-eq (len encoded) u5)  ;; Empty string is type byte + 4 length bytes
+           0x80
+           (let (
+               (encoded-length (- (len encoded) u5))
+               (string-content (unwrap-panic (slice? encoded u5 (len encoded))))
+           )
+               (if (> encoded-length u55)
+                   (let (
+                       (length-bytes (unwrap-panic (to-consensus-buff? encoded-length)))
+                       ;; Take last two bytes for length
+                       (len-byte1 (unwrap-panic (element-at? length-bytes u15)))
+                       (len-byte2 (unwrap-panic (element-at? length-bytes u16)))
+                       (prefix (concat 0xb9 (concat len-byte1 len-byte2)))
+                   )
+                   (check_length (concat prefix string-content)))
+                   (let (
+                       (id (unwrap-panic (to-consensus-buff? (+ u128 encoded-length))))
+                       (prefix (unwrap-panic (element-at? id u16)))
+                       (res (concat prefix string-content))
+                   )
+                   (check_length res))
+               )
+           )
+       )
+   )
 )
 
 (define-private (encode-uint-raw (data uint))
@@ -35,9 +38,14 @@
         0x80
         (let (
             (encoded (unwrap-panic (to-consensus-buff? data)))
-            (sliced (unwrap-panic (slice? encoded u4 (len encoded))))
+            (sliced (unwrap-panic (slice? encoded 
+                (if (>= data (pow u256 u15)) u1 u4)  ;; u1 for very large numbers, u4 for others
+                (len encoded))))
+            (stripped (fold rm-lead sliced 0x00))
             )
-            (check_length (fold rm-lead sliced 0x00))
+            (if (>= data u256)
+                (check_length (concat 0x00 stripped))
+                stripped)
         )
     )
 )
@@ -45,37 +53,37 @@
 (define-read-only (encode-uint (data uint))
     ;; 256^16 is the upper bound
     (if (>= data (pow u256 u15))
-        (check_length (concat 0x90 (encode-uint-raw data)))
+        (check_length (concat 0x91 (encode-uint-raw data)))
         (if (>= data (pow u256 u14))
-            (check_length (concat 0x8f (encode-uint-raw data)))
+            (check_length (concat 0x90 (encode-uint-raw data)))
             (if (>= data (pow u256 u13))
-                (check_length (concat 0x8e (encode-uint-raw data)))
+                (check_length (concat 0x8f (encode-uint-raw data)))
                 (if (>= data (pow u256 u12))
-                    (check_length (concat 0x8d (encode-uint-raw data)))
+                    (check_length (concat 0x8e (encode-uint-raw data)))
                     (if (>= data (pow u256 u11))
-                        (check_length (concat 0x8c (encode-uint-raw data)))
+                        (check_length (concat 0x8d (encode-uint-raw data)))
                         (if (>= data (pow u256 u10))
-                            (check_length (concat 0x8b (encode-uint-raw data)))
+                            (check_length (concat 0x8c (encode-uint-raw data)))
                             (if (>= data (pow u256 u9))
-                                (check_length (concat 0x8a (encode-uint-raw data)))
+                                (check_length (concat 0x8b (encode-uint-raw data)))
                                 (if (>= data (pow u256 u8))
-                                    (check_length (concat 0x89 (encode-uint-raw data)))
+                                    (check_length (concat 0x8a (encode-uint-raw data)))
                                     (if (>= data (pow u256 u7))
-                                        (check_length (concat 0x88 (encode-uint-raw data)))
+                                        (check_length (concat 0x89 (encode-uint-raw data)))
                                         (if (>= data (pow u256 u6))
-                                            (check_length (concat 0x87 (encode-uint-raw data)))
+                                            (check_length (concat 0x88 (encode-uint-raw data)))
                                             (if (>= data (pow u256 u5))
-                                                (check_length (concat 0x86 (encode-uint-raw data)))
+                                                (check_length (concat 0x87 (encode-uint-raw data)))
                                                 (if (>= data (pow u256 u4))
-                                                    (check_length (concat 0x85 (encode-uint-raw data)))
+                                                    (check_length (concat 0x86 (encode-uint-raw data)))
                                                     (if (>= data (pow u256 u3))
-                                                        (check_length (concat 0x84 (encode-uint-raw data)))
+                                                        (check_length (concat 0x85 (encode-uint-raw data)))
                                                         (if (>= data (pow u256 u2))
-                                                            (check_length (concat 0x83 (encode-uint-raw data)))
+                                                            (check_length (concat 0x84 (encode-uint-raw data)))
                                                             (if (>= data u256)
-                                                                (check_length (concat 0x82 (encode-uint-raw data)))
+                                                                (check_length (concat 0x83 (encode-uint-raw data)))
                                                                 (if (>= data u128)
-                                                                    (check_length (concat 0x81 (encode-uint-raw data)))
+                                                                    (check_length (concat 0x82 (encode-uint-raw data)))
                                                                     (encode-uint-raw data)))))))))))))))))
 )
 
@@ -85,20 +93,13 @@
 )
 
 (define-private (encode-buff-arr (objects (list 500 (buff 1024))))
-    (fold concat-buff (map encode-buff objects) 0x)
+    (fold concat-buff objects 0x)
 )
 
 (define-private (encode-buff-long (data (buff 1024)))
   (let
         ((prefix (unwrap-panic  (element-at?  (unwrap-panic (to-consensus-buff? (+ u128 (len data))))  u16) )))
         (check_length (concat prefix data))
-    )
-)
-
-(define-read-only (encode-buff (data (buff 1024)))
-    (if (< u1 (len data))
-      (encode-buff-long data)
-      data
     )
 )
 
--- contracts/stacks/tests/rlp.test.ts
+++ contracts/stacks/tests/rlp.test.ts
@@ -4,90 +4,176 @@ import { describe, expect, it } from "vitest";
 const accounts = simnet.getAccounts();
 const address1 = accounts.get("wallet_1")!;
 
-describe("RLP Encoding Tests", () => {
-  it("encodes an empty string", () => {
+describe("RLP Soroban Compatibility Tests", () => {
+  // test_encode_u8()
+  it("encodes u8", () => {
+    const result = simnet.callReadOnlyFn(
+      "rlp-encode",
+      "encode-uint",
+      [Cl.uint(100)],
+      address1
+    );
+    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
+    expect(result.result.buffer).toEqual(Cl.bufferFromHex("64").buffer);
+  });
+
+  // test_encode_u32()
+  it("encodes u32", () => {
+    const result = simnet.callReadOnlyFn(
+      "rlp-encode",
+      "encode-uint",
+      [Cl.uint(2000022458)],
+      address1
+    );
+    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
+    expect(result.result.buffer).toEqual(Cl.bufferFromHex("85007735EBBA").buffer);
+  });
+
+  // test_encode_u64()
+  it("encodes u64", () => {
+    let result = simnet.callReadOnlyFn(
+      "rlp-encode",
+      "encode-uint",
+      [Cl.uint(1999999999999999999n)],
+      address1
+    );
+    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
+    expect(result.result.buffer).toEqual(Cl.bufferFromHex("89001BC16D674EC7FFFF").buffer);
+
+    result = simnet.callReadOnlyFn(
+      "rlp-encode",
+      "encode-uint",
+      [Cl.uint(199999999)],
+      address1
+    );
+    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
+    expect(result.result.buffer).toEqual(Cl.bufferFromHex("85000BEBC1FF").buffer);
+  });
+
+  // test_encode_u128()
+  it("encodes u128", () => {
+    const result = simnet.callReadOnlyFn(
+      "rlp-encode",
+      "encode-uint", 
+      [Cl.uint(199999999999999999999999999999999999999n)],
+      address1
+    );
+    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
+    expect(result.result.buffer).toEqual(
+      Cl.bufferFromHex("910096769950B50D88F41314447FFFFFFFFF").buffer
+    );
+  });
+
+  // test_encode_string_with_smaller_bytes_length()
+  it("encodes string with smaller bytes length", () => {
     const result = simnet.callReadOnlyFn(
       "rlp-encode",
       "encode-string",
-      [Cl.stringAscii("")],
+      [Cl.stringAscii("soroban-rlp")],
       address1
     );
-    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'. Property 'buffer' does not exist on type 'TrueCV'.
-    expect(result.result.buffer).toEqual(Cl.bufferFromHex("80").buffer);
-  });
-
-  it("encodes short strings", () => {
-    const result = simnet.callReadOnlyFn(
-      "rlp-encode",
-      "encode-string",
-      [Cl.stringAscii("dog")],
-      address1
+    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
+    expect(result.result.buffer).toEqual(
+      Cl.bufferFromHex("8b736f726f62616e2d726c70").buffer // 8b = 139
     );
-    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'. Property 'buffer' does not exist on type 'TrueCV'.
-    expect(result.result.buffer).toEqual(Cl.bufferFromHex("83646f67").buffer);
   });
 
-  it("encodes long strings", () => {
-    const str = "Lorem ipsum dolor sit amet, consectetur adipisicing elit";
+  // test_encode_string_with_larger_bytes_length()
+  it("encodes string with larger bytes length", () => {
+    const str = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s";
     const result = simnet.callReadOnlyFn(
       "rlp-encode",
       "encode-string",
       [Cl.stringAscii(str)],
       address1
     );
-    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'. Property 'buffer' does not exist on type 'TrueCV'.
+    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
     expect(result.result.buffer).toEqual(
-      Cl.bufferFromHex("b8384c6f72656d20697073756d20646f6c6f722073697420616d65742c20636f6e7365637465747572206164697069736963696e6720656c6974").buffer
+      Cl.bufferFromHex("b90097" + // 185 = 0xb9, followed by length bytes 0x00 0x97
+        Buffer.from(str).toString('hex')).buffer
     );
   });
 
-  it("encodes integers", () => {
-    const tests = [
-      { input: 0, expected: "80" },
-      { input: 1, expected: "01" },
-      { input: 16, expected: "10" },
-      { input: 79, expected: "4f" },
-      { input: 127, expected: "7f" },
-      { input: 128, expected: "8180" },
-      { input: 1000, expected: "8203e8" },
-      { input: 100000, expected: "830186a0" }
-    ];
-
-    for (const test of tests) {
-      const result = simnet.callReadOnlyFn(
-        "rlp-encode",
-        "encode-uint",
-        [Cl.uint(test.input)],
-        address1
-      );
-      // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'. Property 'buffer' does not exist on type 'TrueCV'.
-      expect(result.result.buffer).toEqual(Cl.bufferFromHex(test.expected).buffer);
-    }
-  });
-
-  it("encodes an empty list", () => {
+  // test_encode_list_empty()
+  it("encodes empty list", () => {
     const result = simnet.callReadOnlyFn(
-      "rlp-encode",
+      "rlp-encode", 
       "encode-arr",
       [Cl.list([])],
       address1
     );
-    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'. Property 'buffer' does not exist on type 'TrueCV'.
+    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
     expect(result.result.buffer).toEqual(Cl.bufferFromHex("c0").buffer);
   });
 
-  it("encodes string lists", () => {
+  // test_encode_strings()
+  it("encodes strings", () => {
     const result = simnet.callReadOnlyFn(
       "rlp-encode",
       "encode-arr",
       [Cl.list([
-        Cl.bufferFromAscii("dog"),
-        Cl.bufferFromAscii("god"),
-        Cl.bufferFromAscii("cat")
+        Cl.buffer(simnet.callReadOnlyFn(
+          "rlp-encode",
+          "encode-string",
+          [Cl.stringAscii("alice")],
+          address1
+          // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
+        ).result.buffer),
+        Cl.buffer(simnet.callReadOnlyFn(
+          "rlp-encode",
+          "encode-string",
+          [Cl.stringAscii("bob")],
+          address1
+          // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
+        ).result.buffer)
       ])],
       address1
     );
-    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'. Property 'buffer' does not exist on type 'TrueCV'.
-    expect(result.result.buffer).toEqual(Cl.bufferFromHex("cc83646f6783676f6483636174").buffer);
+    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
+    expect(result.result.buffer).toEqual(
+      Cl.bufferFromHex("ca85616c69636583626f62").buffer // Length prefix 0xca = 192 + 10
+    );
+    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
+    expect(result.result.buffer.length).toEqual(11);
+  });
+
+  // test_encode_strings_with_longer_bytes()
+  it("encodes strings with longer bytes", () => {
+    const strings = [
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Egestas maecenas pharetra convallis posuere morbi. Velit laoreet id donec ultrices tincidunt arcu non sodales neque."
+    ];
+
+    const encodedStrings = strings.map(str => 
+      simnet.callReadOnlyFn(
+        "rlp-encode",
+        "encode-string",
+        [Cl.stringAscii(str)],
+        address1
+      )
+    );
+
+    const result = simnet.callReadOnlyFn(
+      "rlp-encode",
+      "encode-arr",
+      // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
+      [Cl.list(encodedStrings.map(e => Cl.buffer(e.result.buffer)))],
+      address1
+    );
+
+    // From Soroban test:
+    // rlp_byte = 0xf7 + 3
+    // Followed by length bytes [0x00, 0x01, 0x74]
+    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
+    expect(result.result.buffer).toEqual(
+      Cl.bufferFromHex("fa000174" + 
+        "b9007c" + Buffer.from(strings[0]).toString('hex') +
+        "b9007b" + Buffer.from(strings[1]).toString('hex') +
+        "b90074" + Buffer.from(strings[2]).toString('hex')
+      ).buffer
+    );
+    // @ts-ignore: Property 'buffer' does not exist on type 'ClarityValue'.
+    expect(result.result.buffer.length).toEqual(376);
   });
 });
